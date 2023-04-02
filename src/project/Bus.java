9d01172c09fcff2aa3728df6cac0da6d7df0cf40
Lenovo <Lenovo@DESKTOP-C7AGLF2>
2023-04-02 14:50:51 +0530

    Commit

---
+++ src/project/Bus.java
@@ -0,0 +1,28 @@
+package project;
+
+public class Bus {
+    private int busNo;
+    private boolean ac;
+    private int capacity;
+	public Bus(int busNo, boolean ac, int capacity) {
+		this.busNo = busNo;
+		this.ac = ac;
+		this.capacity = capacity; }
+	
+	public int getBusNo() { //accessor method
+		return busNo; }
+	
+	public boolean isAc() { //mutator
+		return ac; }
+	public void setAc(boolean ac) {
+		this.ac = ac; }
+	
+	public int getCapacity() {
+		return capacity; }
+	public void setCapacity(int capacity) {
+		this.capacity = capacity; }
+	
+    public void displayBusInfo(){
+			System.out.println("Bus No: " + busNo + "  Ac: " + ac + "  Capacity: " + capacity);    }
+
+}
